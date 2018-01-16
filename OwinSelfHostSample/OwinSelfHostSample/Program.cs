a10ecee9c485151ea8f4060313a62d374b1cf9c4
usmetanina <usmetanina96@mail.ru>
2018-01-16 21:26:48 +0500

    наработки с пары, реализованы нужные методы на ноде

---
+++ OwinSelfHostSample/OwinSelfHostSample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using System.Net.Http;
+using System.IO;
+
+
+namespace OwinSelfHostSample
+{
+    class Programx
+    {
+
+        public static Dictionary<int, string> getDB(string path)
+        {
+            var arr = File.ReadLines(path)
+                            .Select(line => line.Split(';'))
+                            .Where( split => split[0] != "Id" )
+                            .ToDictionary(split => int.Parse(split[0]), split => split[1]);
+            return arr;
+            //return (Dictionary<int, string>)arr.Where(a => a.Value != "deleted");
+
+        }
+        static void Main(string[] args)
+        {
+            ValuesController.val = getDB(ValuesController.pathLog);
+
+            ValuesController.val = ValuesController.val.Concat(getDB(ValuesController.pathDB))
+                       .ToDictionary(x => x.Key, x => x.Value);
+
+            string baseAddress = "http://localhost:9000/";
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine("Web Server is running.");
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+                // Create HttpCient and make a request to api/values 
+                //HttpClient client = new HttpClient();
+
+                //var response = client.GetAsync(baseAddress + "values").Result;
+
+                //Console.WriteLine(response);
+                //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                //Console.ReadLine();
+            }
+        }
+    }
+}
+
+
---
+++ OwinSelfHostSample/OwinSelfHostSample/ValuesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web.Http;
+using System.IO;
+
+namespace OwinSelfHostSample
+{
+    public class ValuesController : ApiController
+    {
+        public static string pathLog = "logDB.txt";
+        public static string pathDB = "fileDB.txt";
+
+        public static Dictionary<int, string> val = 
+            new Dictionary<int, string>();
+               
+        // GET /values 
+        public Dictionary<int, string> Get()
+        {
+            return val;
+        }
+
+        // GET /values/5 
+        public string Get(int id)
+        {
+            Console.WriteLine(id);
+            if (!val.ContainsKey(id) || val[id]=="deleted")
+            {
+                return "404 Not Found";
+            }
+            else
+            {
+                return val[id];
+            }
+        }
+
+        // POST /values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT /values/5 
+        public string Put(int id, [FromBody]string value)
+        {
+            val[id] = value;
+            if (val.Count % 5 == 0)
+                WriteDictToFile();
+            else
+                WriteDictToLog(id, value);
+            return "200 OK";
+        }
+
+        // DELETE /values/5 
+        public void Delete(int id)
+        {
+            val.Remove(id);
+            WriteDictToLog(id,"deleted");
+        }
+
+        private void WriteDictToFile()
+        {
+            //System.IO.File.AppendAllText(pathLog, String.Empty);
+            if (File.Exists(pathLog))
+            {
+                StreamWriter Writer = new StreamWriter(pathLog, false, Encoding.UTF8);
+                Writer.WriteLine("");
+                Writer.Close();
+            }
+
+            StreamWriter file = new StreamWriter(pathDB);
+            lock (file)
+            {           
+                using (file)
+                    foreach (var entry in val)
+                        file.WriteLine("{0};{1}", entry.Key, entry.Value);
+           }
+        }
+        private void WriteDictToLog(int key, string value)
+        {
+            using (StreamWriter sw = File.AppendText(pathLog))
+            {
+                sw.WriteLine("{0};{1}", key, value);
+            }	
+        }
+    }
+}
