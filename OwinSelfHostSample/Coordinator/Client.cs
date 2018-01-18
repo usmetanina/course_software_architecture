2304d016685e416b799334bd2b8f17c550b0c60c
usmetanina <usmetanina96@mail.ru>
2018-01-19 01:09:19 +0500

    рефакторинг. вынесла в отдельный класс отправление запросов

---
+++ OwinSelfHostSample/Coordinator/Client.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Coordinator
+{
+    public class Client
+    {
+        public string url = "http://localhost:9000";
+        HttpClient client = new HttpClient();
+
+        public string Get()
+        {
+            var responses = client.GetAsync(url).Result;
+            return responses.Content.ReadAsStringAsync().Result;
+        }
+
+        public string Get(int id)
+        {
+            var responses = client.GetAsync(url).Result;
+            return responses.Content.ReadAsStringAsync().Result;
+        }
+
+        public string Put(int id, [FromBody]string value)
+        {
+            var jsonString = JsonConvert.SerializeObject(value);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var response = client.PutAsync(url, content).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public string Delete(int id)
+        {
+            var deleted = client.DeleteAsync(url).Result;
+            return deleted.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
--- OwinSelfHostSample/Coordinator/ValuesController.cs
+++ OwinSelfHostSample/Coordinator/ValuesController.cs
@@ -14,38 +14,35 @@ namespace OwinSelfHostSample
 {
     public class ValuesController : ApiController
     {
-        string url = "http://localhost:";
-        HttpClient client = new HttpClient();
-
+        Client client = new Client();
         private string ShardFunction(int key)
         {
-            Console.WriteLine("Node ports " + Storage.nodePorts[0] + Storage.nodePorts[1]);
+
+            //Console.WriteLine("Node ports " + Storage.nodePorts[0] + Storage.nodePorts[1]);
             return Storage.nodePorts[key % int.Parse(Storage.countNode)];
         }
 
         // GET /values/5 
         public string Get(int id)
         {
-            Console.WriteLine("Shadr go " + ShardFunction(id));
-            var responses = client.GetAsync(url + ShardFunction(id) + "/values/").Result;
-            return responses.Content.ReadAsStringAsync().Result;
+            client.url = "http://localhost:" + ShardFunction(id) + "/values/" + id;
+            //Console.WriteLine("Shadr go " + ShardFunction(id));
+            return client.Get(id);
         }
 
         // PUT /values/5 
         public string Put(int id, [FromBody]string value)
         {
-            Console.WriteLine("Shadr go " + ShardFunction(id));
-            var jsonString = JsonConvert.SerializeObject(value);
-            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-            var response = client.PutAsync(url + ShardFunction(id) + "/values/" + id, content).Result;
-            return response.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine("Shadr go " + ShardFunction(id));
+            client.url = "http://localhost:" + ShardFunction(id) + "/values/" + id;
+            return client.Put(id, value);
         }
 
         // DELETE /values/5 
         public string Delete(int id)
         {
-            var deleted = client.DeleteAsync(url + ShardFunction(id) + "/values/" + id).Result;
-            return deleted.Content.ReadAsStringAsync().Result;
+            client.url = "http://localhost:" + ShardFunction(id) + "/values/" + id;
+            return client.Delete(id);
         }
     }
 }
